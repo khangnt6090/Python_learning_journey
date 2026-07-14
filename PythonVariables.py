c095b3b605e3d59abe7dc81ed40ac64a55adb52b
Khang <khangnt6090@ut.edu.vn>
2026-07-14 15:15:37 +0700

    Đã học bài học mới về các biến trong Python

---
+++ PythonVariables.py
@@ -0,0 +1,46 @@
+#String
+first_name = "Nguyen"
+food = "Hu tieu"
+email = "noobvnkhang@gmail.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+
+#Integer
+age = 20
+quantity = 3
+num_of_students = 60
+
+print(f"You are {age} years old")
+print(f"You want to buy {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+#Float
+price = 10.99
+gpa = 3.3
+distance = 5.5
+
+print(f"The price of the item is ${price}")
+print(f"Your GPA is {gpa}")
+print(f"You ran {distance} km")
+
+#Boolean
+is_student = True
+for_sale = False
+is_online = True
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
+
+if for_sale:
+    print("The item is for sale")
+else:
+    print("The item is not for sale")
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")
