fcc44cc5f444923aad6b1efd6feb6d7cc37e970c
Khang <khangnt6090@ut.edu.vn>
2026-08-11 15:15:17 +0700

    Chương trình đếm ngược thời gian

---
+++ CountdownTimerProgram.py
@@ -0,0 +1,12 @@
+import time
+
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600) % 24
+    print(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
+    time.sleep(1) #làm chương trình tạm dừng trong 1 giây
+
+print("TIME'S UP!")
